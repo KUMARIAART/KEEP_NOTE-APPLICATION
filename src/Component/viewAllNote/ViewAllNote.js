ba0febf571bc4ae6b49f1a52247b5bf504b17caa
Ubuntu <ubuntu@ip-172-31-38-10.ap-south-1.compute.internal>
2023-03-01 11:30:37 +0000

    fourth commit

--- src/Component/viewAllNote/ViewAllNote.js
+++ src/Component/viewAllNote/ViewAllNote.js
@@ -1,7 +1,36 @@
-const ViewAllNote=()=>{
-    return(
-        <>
-        </>
+import React, { useState, useEffect } from 'react';
+const ViewAllNote = () => {
+    const [showData, setShowData] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/Notes")
+            .then((res) => res.json())
+            .then((value) => {
+                setShowData(value)
+            })
+
+    }, []);
+    return (
+        <div>
+            <thead>
+               <tr>
+                 <th>Note Detail</th>
+                 <th>Due Date</th>
+                 <th>Category</th>
+               </tr>
+          </thead>
+            {showData.map((item) =>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>{item.note}</td>
+                            <td>{item.date}</td>
+                            <td>{item.category}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            )}
+        </div>
     );
 }
 export default ViewAllNote;
